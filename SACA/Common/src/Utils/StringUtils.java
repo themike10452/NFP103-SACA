ef9059ac4e52208ce7e5c6c3a1e3922adcd24a62
Mike Mouawad <thephenommvp@gmail.com>
2017-08-04 23:19:25 +0300

    StringUtils: added base64 encoding

--- SACA/Common/src/Utils/StringUtils.java
+++ SACA/Common/src/Utils/StringUtils.java
@@ -1,5 +1,7 @@
 package Utils;
 
+import java.util.Base64;
+
 /**
  * Created by Mike on 7/16/2017.
  */
@@ -11,6 +13,18 @@ public class StringUtils {
 
     public static boolean isNullOrEmpty(String str) {
         return str == null || str.length() == 0;
+    }
+
+    public static String toBase64(String str) {
+        return str != null
+                ? new String(Base64.getEncoder().encode(str.getBytes()))
+                : null;
+    }
+
+    public static String fromBase64(String str) {
+        return str != null
+                ? new String(Base64.getDecoder().decode(str))
+                : null;
     }
 
 }
