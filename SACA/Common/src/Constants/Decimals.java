0f775904ce0e958970c170ab672d808217b82532
Mike Mouawad <thephenommvp@gmail.com>
2017-09-04 20:45:18 +0300

    map scale adjustment

--- SACA/Common/src/Constants/Decimals.java
+++ SACA/Common/src/Constants/Decimals.java
@@ -1,7 +1,7 @@
 package Constants;
 
 public class Decimals {
-    public static final float MAP_SCALE = 1.6f;
+    public static final float MAP_SCALE = 4.64f;
     public static final float AIRPLANE_MAX_PITCH = 20.0f;
     public static final float AIRPLANE_MAX_ROLL = 80.0f;
     public static final float AIRPLANE_MAX_SPEED = 926.0f;
